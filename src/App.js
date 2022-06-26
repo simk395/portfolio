d6db2af77dd9defae141ca18f0a899efb2a042a5
Simon Kong <skongnyc@gmail.com>
2022-06-25 23:01:04 -0400

    decided on tabs instead of routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Tab, Tabs, Nav, Row, Col} from 'react-bootstrap'
+import Home from './Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tab.Container defaultActiveKey="home">
+        <Row>
+          <Nav>
+            <Nav.Item>
+              <Nav.Link eventKey="home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="ms-auto">
+              <Nav.Link eventKey="projects">Projects</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="contributions">Contributions</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Row>
+        <Row>
+          <Tab.Content>
+            <Tab.Pane eventKey="home">
+              <Home/>
+            </Tab.Pane>
+            <Tab.Pane eventKey="projects">
+              projects
+            </Tab.Pane>
+            <Tab.Pane eventKey="contributions">
+              contribtions
+            </Tab.Pane>
+          </Tab.Content>
+        </Row>
+      </Tab.Container>
     </div>
   );
 }
